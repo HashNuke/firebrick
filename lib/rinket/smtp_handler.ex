065bad921b0daa26aa4833a26db81700ef7b901b
Akash Manohar J <akash@akash.im>
2013-10-29 13:27:15 +0530

    Ported all required functions to Elixir

--- lib/rinket/smtp_handler.ex
+++ lib/rinket/smtp_handler.ex
@@ -1,5 +1,5 @@
 defmodule SmtpHandler do
-  @behaviour :gen_smtp_server_session
+  # @behaviour :gen_smtp_server_session
 
 
 # -export([init/4, handle_HELO/2, handle_EHLO/3, handle_MAIL/2, handle_MAIL_extension/2,
@@ -12,7 +12,7 @@ defmodule SmtpHandler do
     record_type options: list
   end
 
-  @type error_message :: {'error', string, State[]}
+  @type error_message :: {:error, String.t, State.t}
 
 # %% @doc Initialize the callback module's state for a new session.
 # %% The arguments to the function are the SMTP server's hostname (for use in the SMTP anner),
@@ -26,7 +26,7 @@ defmodule SmtpHandler do
 # %% to ALL subsequent calls to the callback module, so it can be used to keep track of the SMTP
 # %% session. You can also return `{stop, Reason, Message}' where the session will exit with Reason
 # %% and send Message to the client.
-  @spec init(binary, non_neg_integer, tuple, list), do: {:ok, string, State} | {:stop, any, string}
+  @spec init(binary, non_neg_integer, tuple, list) :: {:ok, String.t, State.t} | {:stop, any, String.t}
   def init(hostname, session_count, address, options) do
     :io.format("peer: ~p~n", [address])
     case session_count > 20 do
@@ -49,7 +49,7 @@ defmodule SmtpHandler do
   # %% , for example, by looking at the IP address passed in to the init function) and the new callback
   # %% state. You can reject the HELO by returning `{error, Message, State}' and the Message will be
   # %% sent back to the client. The reject message MUST contain the SMTP status code, eg. 554.
-  @spec handle_HELO(binary, State[]), do: {:ok, pos_integer, State} | {:ok, State} | error_message
+  @spec handle_HELO(binary, State[]) :: {:ok, pos_integer, State} | {:ok, State} | error_message
   def handle_HELO("invalid", state) do
     # contrived example
     {:error, "554 invalid hostname", state}
@@ -60,66 +60,80 @@ defmodule SmtpHandler do
   end
 
   def handle_HELO(hostname, state) do
-    :io.format("HELO from ~s~n", [hostname]),
-    {:ok, 655360, state}. # 640kb of HELO should be enough for anyone.
+    :io.format("HELO from ~s~n", [hostname])
+    {:ok, 655360, state} # 640kb of HELO should be enough for anyone.
     # If {ok, state} was returned here, we'd use the default 10mb limit
   end
 
 
-# %% @doc Handle the EHLO verb from the client. As with EHLO the hostname is provided as an argument,
-# %% but in addition to that the list of ESMTP Extensions enabled in the session is passed. This list
-# %% of extensions can be modified by the callback module to add/remove extensions.
-# %%
-# %% The return values are `{ok, Extensions, State}' where Extensions is the new list of extensions
-# %% to use for this session or `{error, Message, State}' where Message is the reject message as
-# %% with handle_HELO.
-# -spec handle_EHLO(Hostname :: binary(), Extensions :: list(), State :: #state{}) -> {'ok', list(), #state{}} | error_message().
-# handle_EHLO(<<"invalid">>, _Extensions, State) ->
-#   % contrived example
-#   {error, "554 invalid hostname", State};
-# handle_EHLO(Hostname, Extensions, State) ->
-#   io:format("EHLO from ~s~n", [Hostname]),
-#   % You can advertise additional extensions, or remove some defaults
-#   MyExtensions = case proplists:get_value(auth, State#state.options, false) of
-#     true ->
-#       % auth is enabled, so advertise it
-#       Extensions ++ [{"AUTH", "PLAIN LOGIN CRAM-MD5"}, {"STARTTLS", true}];
-#     false ->
-#       Extensions
-#   end,
-#   {ok, MyExtensions, State}.
+  # %% @doc Handle the EHLO verb from the client. As with EHLO the hostname is provided as an argument,
+  # %% but in addition to that the list of ESMTP Extensions enabled in the session is passed. This list
+  # %% of extensions can be modified by the callback module to add/remove extensions.
+  # %%
+  # %% The return values are `{ok, Extensions, State}' where Extensions is the new list of extensions
+  # %% to use for this session or `{error, Message, State}' where Message is the reject message as
+  # %% with handle_HELO.
+  @spec handle_EHLO(binary, list, State.t) :: {:ok, list, State.t} | error_message
+  def handle_EHLO("invalid", _extensions, state) do
+    # contrived example
+    {:error, "554 invalid hostname", state}
+  end
 
-# %% @doc Handle the MAIL FROM verb. The From argument is the email address specified by the
-# %% MAIL FROM command. Extensions to the MAIL verb are handled by the `handle_MAIL_extension'
-# %% function.
-# %%
-# %% Return values are either `{ok, State}' or `{error, Message, State}' as before.
-# -spec handle_MAIL(From :: binary(), State :: #state{}) -> {'ok', #state{}} | error_message().
-# handle_MAIL(<<"badguy@blacklist.com">>, State) ->
-#   {error, "552 go away", State};
-# handle_MAIL(From, State) ->
-#   io:format("Mail from ~s~n", [From]),
-#   % you can accept or reject the FROM address here
-#   {ok, State}.
+  def handle_EHLO(hostname, extensions, state) do
+    :io.format("EHLO from ~s~n", [hostname])
+    # You can advertise additional extensions, or remove some defaults
+    my_extensions = case :proplists.get_value(:auth, state.options, false) do
+      true ->
+        # auth is enabled, so advertise it
+        extensions ++ [{"AUTH", "PLAIN LOGIN CRAM-MD5"}, {"STARTTLS", true}]
+      false ->
+        extensions
+    end
+    {:ok, my_extensions, state}
+  end
 
-# %% @doc Handle an extension to the MAIL verb. Return either `{ok, State}' or `error' to reject
-# %% the option.
-# -spec handle_MAIL_extension(Extension :: binary(), State :: #state{}) -> {'ok', #state{}} | 'error'.
-# handle_MAIL_extension(<<"X-SomeExtension">> = Extension, State) ->
-#   io:format("Mail from extension ~s~n", [Extension]),
-#   % any MAIL extensions can be handled here
-#   {ok, State};
-# handle_MAIL_extension(Extension, _State) ->
-#   io:format("Unknown MAIL FROM extension ~s~n", [Extension]),
-#   error.
 
-# -spec handle_RCPT(To :: binary(), State :: #state{}) -> {'ok', #state{}} | {'error', string(), #state{}}.
-# handle_RCPT(<<"nobody@example.com">>, State) ->
-#   {error, "550 No such recipient", State};
-# handle_RCPT(To, State) ->
-#   io:format("Mail to ~s~n", [To]),
-#   % you can accept or reject RCPT TO addesses here, one per call
-#   {ok, State}.
+  # %% @doc Handle the MAIL FROM verb. The From argument is the email address specified by the
+  # %% MAIL FROM command. Extensions to the MAIL verb are handled by the `handle_MAIL_extension'
+  # %% function.
+  # %%
+  # %% Return values are either `{ok, State}' or `{error, Message, State}' as before.
+  @spec handle_MAIL(binary, State.t) :: {:ok, State.t} | error_message
+  def handle_MAIL("badguy@blacklist.com", state) do
+    { :error, "552 go away", state}
+  end
+
+  def handle_MAIL(from, state) do
+    :io.format("Mail from ~s~n", [from])
+    # you can accept or reject the FROM address here
+    {:ok, state}
+  end
+
+  # @doc Handle an extension to the MAIL verb. Return either `{ok, State}' or `error' to reject
+  # the option.
+  @spec handle_MAIL_extension(binary, State.t) :: {:ok, State.t} | :error
+  #TODO
+  def handle_MAIL_extension("X-SomeExtension" = extension, state) do
+    :io.format("Mail from extension ~s~n", [extension])
+    # any MAIL extensions can be handled here
+    {:ok, state}
+  end
+
+  def handle_MAIL_extension(extension, _state) do
+    :io.format("Unknown MAIL FROM extension ~s~n", [extension])
+    :error
+  end
+
+@spec handle_RCPT(binary(), State.t) :: {:ok, State.t} | {:error, String.t, State.t}
+def handle_RCPT("nobody@example.com", state) do
+  {:error, "550 No such recipient", state}
+end
+
+def handle_RCPT(to, state) do
+  :io.format("Mail to ~s~n", [to])
+  # you can accept or reject RCPT TO addesses here, one per call
+  {:ok, state}
+end
 
 # -spec handle_RCPT_extension(Extension :: binary(), State :: #state{}) -> {'ok', #state{}} | 'error'.
 # handle_RCPT_extension(<<"X-SomeExtension">> = Extension, State) ->
@@ -130,51 +144,63 @@ defmodule SmtpHandler do
 #   io:format("Unknown RCPT TO extension ~s~n", [Extension]),
 #   error.
 
-# -spec handle_DATA(From :: binary(), To :: [binary(),...], Data :: binary(), State :: #state{}) -> {'ok', string(), #state{}} | {'error', string(), #state{}}.
-# handle_DATA(_From, _To, <<>>, State) ->
-#   {error, "552 Message too small", State};
-# handle_DATA(From, To, Data, State) ->
-#   % some kind of unique id
-#   Reference = lists:flatten([io_lib:format("~2.16.0b", [X]) || <<X>> <= erlang:md5(term_to_binary(erlang:now()))]),
-#   % if RELAY is true, then relay email to email address, else send email data to console
-#   case proplists:get_value(relay, State#state.options, false) of
-#     true -> relay(From, To, Data);
-#     false ->
-#       io:format("message from ~s to ~p queued as ~s, body length ~p~n", [From, To, Reference, byte_size(Data)]),
-#       case proplists:get_value(parse, State#state.options, false) of
-#         false -> ok;
-#         true ->
-#           try mimemail:decode(Data) of
-#             _Result ->
-#               io:format("Message decoded successfully!~n")
-#           catch
-#             What:Why ->
-#               io:format("Message decode FAILED with ~p:~p~n", [What, Why]),
-#               case proplists:get_value(dump, State#state.options, false) of
-#               false -> ok;
-#               true ->
-#                 %% optionally dump the failed email somewhere for analysis
-#                 File = "dump/"++Reference,
-#                 case filelib:ensure_dir(File) of
-#                   ok ->
-#                     file:write_file(File, Data);
-#                   _ ->
-#                     ok
-#                 end
-#               end
-#           end
-#       end
-#   end,
-#   % At this point, if we return ok, we've accepted responsibility for the email
-#   {ok, Reference, State}.
-
-  @spec handle_RSET(State), do: State
-  def handle_RSET(state) do
-    # reset any relevant internal state
-    state
+  @spec handle_DATA(binary, [binary,...], binary, State.t) :: {:ok, String.t, State.t} | {:error, String.t, State.t}
+  def handle_DATA(_from, _to, "", state) do
+    {:error, "552 Message too small", state}
   end
 
-  @spec handle_VRFY(binary, State), do: {:ok, string, State} | {:error, string, State}
+  def handle_DATA(from, to, data, state) do
+    # some kind of unique id
+    ref_list = Kernel.bitstring_to_list(:erlang.md5(term_to_binary(:erlang.now())))
+    reference = :lists.flatten Enum.map(ref_list, fn(n)-> :io_lib.format("~2.16.0b", [n]) end)
+
+    IO.inspect "TODO Still cant handle data"
+    IO.inspect data
+
+    # if RELAY is true, then relay email to email address, else send email data to console
+    # case :proplists.get_value(relay, state.options, false) do
+    #   true  -> relay(from, to, data)
+    #   false ->
+
+    #     :io.format("message from ~s to ~p queued as ~s, body length ~p~n", [from, to, reference, byte_size(data)])
+    #     case :proplists.get_value(parse, state.options, false) do
+    #       false -> :ok
+    #       true ->
+    #         try do
+    #           result = :mimemail.decode(data)
+    #           :io.format("Message decoded successfully!~n")
+    #         rescue
+    #           [reason] ->
+    #             :io.format("Message decode FAILED with ~p:~n", [reason])
+    #             case :proplists.get_value(dump, state.options, false) do
+    #             false -> :ok
+    #             true ->
+    #               # optionally dump the failed email somewhere for analysis
+    #               file = "dump/#{reference}",
+    #               case :filelib.ensure_dir(file) do
+    #                 :ok ->
+    #                   :file.write_file(file, data)
+    #                 _ ->
+    #                   :ok
+    #               end
+    #             end
+    #           end
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
+
+    # At this point, if we return ok, we've accepted responsibility for the email
+    {:ok, reference, state}
+  end
+
+  @spec handle_RSET(State.t) :: State.t
+  def handle_RSET(state) do
+    state # reset any relevant internal state
+  end
+
+  @spec handle_VRFY(binary, State.t) :: {:ok, String.t, State.t} | {:error, String.t, State.t}
   def handle_VRFY("someuser", state) do
     {:ok, "someuser@#{:smtp_util.guess_FQDN()}", state}
   end
@@ -183,41 +209,46 @@ defmodule SmtpHandler do
     {:error, "252 VRFY disabled by policy, just send some mail", state}
   end
 
-  @spec handle_other(binary, binary, State), do: {string, State}
+  @spec handle_other(binary, binary, State.t) :: {String.t, State.t}
   def handle_other(verb, _args, state) do
     # You can implement other SMTP verbs here, if you need to
-    {["500 Error: command not recognized : '", verb, "'"], state}.
+    {["500 Error: command not recognized : '", verb, "'"], state}
   end
 
-# %% this callback is OPTIONAL
-# %% it only gets called if you add AUTH to your ESMTP extensions
-# -spec handle_AUTH(Type :: 'login' | 'plain' | 'cram-md5', Username :: binary(), Password :: binary() | {binary(), binary()}, #state{}) -> {'ok', #state{}} | 'error'.
-# handle_AUTH(Type, <<"username">>, <<"PaSSw0rd">>, State) when Type =:= login; Type =:= plain ->
-#   {ok, State};
-# handle_AUTH('cram-md5', <<"username">>, {Digest, Seed}, State) ->
-#   case smtp_util:compute_cram_digest(<<"PaSSw0rd">>, Seed) of
-#     Digest ->
-#       {ok, State};
-#     _ ->
-#       error
-#   end;
-# handle_AUTH(_Type, _Username, _Password, _State) ->
-#   error.
+  # this callback is OPTIONAL
+  # it only gets called if you add AUTH to your ESMTP extensions
+  # @spec handle_AUTH('login' | 'plain' | 'cram-md5', binary, binary | {binary, binary}, State.t) :: {:ok, State.t} | :error
+  # def handle_AUTH(type, "username", "PaSSw0rd", state) when type =:= login; type =:= plain do
+  #   {ok, state}
+  # end
+
+  def handle_AUTH('cram-md5', "username", {digest, seed}, state) do
+    case :smtp_util.compute_cram_digest("PaSSw0rd", seed) do
+      digest ->
+        {:ok, state}
+      _ ->  # never comes to this, because previous case matches all
+        :error
+    end
+  end
+
+  def handle_AUTH(_type, _username, _password, _state) do
+    :error
+  end
 
   # this callback is OPTIONAL
   # it only gets called if you add STARTTLS to your ESMTP extensions
-  @spec handle_STARTTLS(State), do: State
+  @spec handle_STARTTLS(State.t) :: State.t
   def handle_STARTTLS(state) do
     :io.format("TLS Started~n")
     state
   end
 
-  @spec code_change(any, State, any), do: {:ok, State}
+  @spec code_change(any, State.t, any) :: {:ok, State.t}
   def code_change(_old_vsn, state, _extra) do
     {:ok, state}
   end
 
-  @spec terminate(any, State), do: {:ok, any, State}
+  @spec terminate(any, State.t) :: {:ok, any, State.t}
   def terminate(reason, state) do
     {:ok, reason, state}
   end
