43dc42eb85b2adfcb10007342776c6aeb0629a44
Akash Manohar J <akash@akash.im>
2013-11-26 00:00:49 +0530

    GET /api/mails gets mails

--- web/routers/mails_api_router.ex
+++ web/routers/mails_api_router.ex
@@ -1,8 +1,17 @@
 defmodule MailsApiRouter do
   use Dynamo.Router
+  import Firebrick.RouterUtils
+
+  prepare do
+    conn = authenticate_user!(conn)
+  end
+
 
   get "/" do
-    #TODO
+    user = conn.assigns[:current_user]
+    {threads, count} = Thread.search("user_id:#{user.id}")
+    thread_attributes = lc thread inlist threads, do: thread.public_attributes
+    json_response thread_attributes, conn
   end
 
 
