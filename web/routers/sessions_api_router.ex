d1b008e45a2ebfdae42c4c34dbbad29c1066e086
Akash Manohar J <akash@akash.im>
2014-01-06 20:45:37 +0530

    Refactor login into a seperate function

--- web/routers/sessions_api_router.ex
+++ web/routers/sessions_api_router.ex
@@ -14,27 +14,35 @@ defmodule SessionsApiRouter do
 
 
   post "/" do
-    {:ok, params} = conn.req_body
-    |> JSEX.decode
-
-    {results, count, _} = User.query("type:user AND username:#{params["username"]}")
-
-    if count > 0 do
-      result = results |> hd
-      if User.valid_password?(result, params["password"]) do
-        conn = put_session(conn, :user_id, result.id)
-
-        json_response [user: result.public_attributes], conn
-      else
-        json_response [error: "Please check your login credentials"], conn, 401
-      end
-    else
-      json_response [error: "Maybe you don't have an account?"], conn
-    end
+    params = conn.params
+    login(conn, params["username"], params["password"])
   end
 
 
   delete "/" do
     json_response [ok: "logged out"], delete_session(conn, :user_id)
   end
+
+
+  defp login(conn, username, password) when username == nil or password == nil do
+    json_response [error: "Please check your login credentials."], conn, 401
+  end
+
+  defp login(conn, username, password) do
+    {results, count, _} = User.query("type:user AND username:#{username}")
+
+    case count > 0 do
+      true ->
+        result = results |> hd
+        if User.valid_password?(result, password) do
+          conn = put_session(conn, :user_id, result.id)
+          json_response [user: result.public_attributes], conn
+        else
+          json_response [error: "Please check your login credentials."], conn, 401
+        end
+      false ->
+        json_response [error: "Maybe you don't have an account?"], conn, 401
+    end
+  end
+
 end
