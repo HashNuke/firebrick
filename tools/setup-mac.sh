00110df2fcedd714fbffe8e5df51ebebf7616eb9
Akash Manohar J <akash@akash.im>
2015-06-11 09:14:17 +0530

    Seperate setup and unsetup scripts

--- tools/setup-mac.sh
+++ tools/setup-mac.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "clear" ]; then
-  sudo pfctl -d
-else
-  sudo pfctl -evf $(dirname $0)/smtp-port-forward.pfconf
-fi
+sudo pfctl -evf $(dirname $0)/smtp-port-forward.pfconf
