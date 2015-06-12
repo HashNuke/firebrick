918a628aac10a95912247205e43d7ffb83cfcebc
Akash Manohar J <akash@akash.im>
2015-06-12 08:40:00 +0530

    Add alias_of_id to Mailbox

--- priv/repo/migrations/20150611174004_create_mailboxes_table.exs
+++ priv/repo/migrations/20150611174004_create_mailboxes_table.exs
@@ -6,6 +6,7 @@ defmodule Firebrick.Repo.Migrations.CreateMailboxesTable do
       add :email, :text
       add :domain_id, :integer
       add :mailbox_type, :text
+      add :alias_of_id, :integer
     end
   end
 end
