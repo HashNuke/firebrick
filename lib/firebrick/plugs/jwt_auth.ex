6cbd2b7cd8dd15739d25bd08db5a3834d99c1037
Akash Manohar J <akash@akash.im>
2015-06-12 20:26:28 +0530

    Refactor JwtAuth plug

--- lib/firebrick/plugs/jwt_auth.ex
+++ lib/firebrick/plugs/jwt_auth.ex
@@ -3,20 +3,29 @@ defmodule Firebrick.Plugs.JwtAuth do
 
   alias Plug.Conn
 
-  def init(opts), do: %{}
+
+  def init(opts) do
+    %{}
+  end
 
 
   def call(conn, level) do
     jwt = conn.cookies["jwt"]
-    if jwt == nil do
-      auth_fail!(conn)
-    else
-      case Firebrick.Jwt.decode(jwt) do
-        {:ok, data} ->
-          Conn.assign(conn, :jwt_data, data)
-        {:error, reason} ->
-          auth_fail!(conn)
-      end
+    process_req(conn, jwt)
+  end
+
+
+  defp process_req(conn, nil) do
+    auth_fail!(conn)
+  end
+
+
+  defp process_req(conn, jwt) do
+    case Firebrick.Jwt.decode(jwt) do
+      {:ok, data} ->
+        Conn.assign(conn, :jwt_data, data)
+      {:error, reason} ->
+        auth_fail!(conn)
     end
   end
 
