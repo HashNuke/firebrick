652616a851b1878dc1a50e2f75a0e713e30a7bf7
Akash Manohar J <akash@akash.im>
2015-06-19 18:44:21 +0530

    Check before creating roles or users

--- lib/mix/firebrick/setup.ex
+++ lib/mix/firebrick/setup.ex
@@ -1,18 +1,40 @@
 defmodule Mix.Tasks.Firebrick.Setup do
   use Mix.Task
   import Ecto.Query
+  import Ecto.Model
+  alias Firebrick.Repo
+
 
   @shortdoc "Setup firebrick"
   def run(_) do
     Mix.Task.run "app.start", []
 
+    ensure_roles
+    ensure_admin_user
+  end
+
+
+  def ensure_roles do
     roles = ["admin", "member"]
     Enum.each roles, fn(role_name)->
       query = from role in Firebrick.Role, where: role.name == ^role_name
-      if Firebrick.Repo.all(query) == [] do
-        Firebrick.Repo.insert %Firebrick.Role{name: role_name}
+      if Repo.all(query) == [] do
+        Repo.insert %Firebrick.Role{name: role_name}
       end
     end
+  end
 
+
+  def ensure_admin_user do
+    admin_users = Repo.all(from role in Firebrick.Role, where: role.name == "admin", select: role)
+    |> assoc(:users)
+    |> Repo.all
+
+
+    if admin_users == [] do
+      # TODO create user admin@example.com:password
+      # Create associated mailbox
+      # User.create(email, password)
+    end
   end
 end
