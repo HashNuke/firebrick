bfea9bcc67ed5897940e871cb6577a7b9bed0699
Akash Manohar J <akash@akash.im>
2015-08-16 18:35:28 +0530

    Add model for MailThread route

--- frontend/app/mail-thread/route.js
+++ frontend/app/mail-thread/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model(params) {
+    let mailThread = this.store.find('mail-thread', params.id);
+    return mailThread;
+  }
+
 });
