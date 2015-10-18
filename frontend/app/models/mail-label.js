91e7e1884750b40db5d880f9af09f5b764b83a5a
Akash Manohar J <akash@akash.im>
2015-10-18 08:03:04 +0530

    Disable analytics and generate new ember-cli app

--- frontend/app/models/mail-label.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  labelType: DS.attr('string'),
-
-  user: DS.belongsTo('user'),
-  mailThreads: DS.hasMany('mail-thread')
-});
