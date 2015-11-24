d81604e35a047326d0174f8b3ec29ab222721305
Akash Manohar J <akash@akash.im>
2015-11-24 09:17:14 +0530

    Update faker and comeonin

--- mix.exs
+++ mix.exs
@@ -46,10 +46,10 @@ defmodule Firebrick.Mixfile do
       {:phoenix_html, "~> 2.1"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
-      {:comeonin, "~> 1.1.2"},
+      {:comeonin, "~> 1.6"},
       {:gen_smtp, github: "Vagabond/gen_smtp"},
       {:hound, "~> 0.7", only: :test},
-      {:faker, "~> 0.5", only: :test}
+      {:faker, "~> 0.6", only: :test}
     ]
   end
 end
